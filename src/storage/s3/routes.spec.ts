227c72470fbdf164e8ad0e998fe2a40f4b43cee2
killbasa <git@killbasa.com>
2025-05-10 17:44:53 -0400

    add tests

--- src/storage/s3/routes.spec.ts
+++ src/storage/s3/routes.spec.ts
@@ -3,6 +3,9 @@ import { server } from '../../server.js';
 import { s3Routes } from './routes.js';
 import { storagePreHandlerHook } from '../handlers.js';
 import { ReadStream } from 'node:fs';
+import { flushPromises } from '../../mocks/utils.js';
+import { RECORDINGS_PATH } from '../../lib/utils/constants.js';
+import type { DvrWebhookPayload } from '../../lib/types/srs.js';
 
 const mocks = vi.hoisted(() => {
 	return {
@@ -56,8 +59,208 @@ describe('S3 routes', () => {
 				body
 			});
 
+			await flushPromises();
+
 			expect(response.statusCode).toBe(200);
 			expect(response.json()).toStrictEqual({ code: 0 });
+
+			expect(mocks.s3Upload).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledWith(`${RECORDINGS_PATH}/app_id/stream_id/recording.flv`);
+		});
+
+		test('s3Upload throws error', async () => {
+			mocks.s3Upload.mockRejectedValueOnce(new Error());
+
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body
+			});
+
+			expect(response.statusCode).toBe(200);
+			expect(response.json()).toStrictEqual({ code: 0 });
+
+			expect(mocks.s3Upload).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).not.toHaveBeenCalled();
+		});
+
+		test('rm throws error', async () => {
+			mocks.rm.mockRejectedValue(new Error());
+
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body
+			});
+
+			await flushPromises();
+
+			expect(response.statusCode).toBe(200);
+			expect(response.json()).toStrictEqual({ code: 0 });
+
+			expect(mocks.s3Upload).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledWith(`${RECORDINGS_PATH}/app_id/stream_id/recording.flv`);
+			await expect(mocks.rm).rejects.toThrow();
+		});
+
+		it('should fail if file is already being uploaded', async () => {
+			vi.spyOn(server.tracker, 'has') //
+				.mockReturnValueOnce(false)
+				.mockReturnValueOnce(true);
+
+			const resOne = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body
+			});
+
+			expect(resOne.statusCode).toBe(200);
+			expect(resOne.json()).toStrictEqual({ code: 0 });
+
+			const resTwo = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body
+			});
+
+			expect(resTwo.statusCode).toBe(400);
+			expect(resTwo.json()).toStrictEqual({ code: 1 });
+		});
+	});
+
+	describe('validation', () => {
+		const body = MockBody({
+			app: 'app_id',
+			stream: 'stream_id',
+			file: '/invalid/recording.flv'
+		});
+
+		it('should fail with invalid body', async () => {
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body: {
+					key: 'value'
+				}
+			});
+
+			expect(response.statusCode).toBe(400);
+			expect(response.json()).toStrictEqual({ code: 1 });
+		});
+
+		it('should fail with invalid path root', async () => {
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body: {
+					...body,
+					file: '/invalid/recording.flv'
+				} satisfies DvrWebhookPayload
+			});
+
+			expect(response.statusCode).toBe(400);
+			expect(response.json()).toStrictEqual({ code: 1 });
+
+			expect(mocks.existsSync).not.toHaveBeenCalled();
+		});
+
+		it('should fail with invalid file extension', async () => {
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body: {
+					...body,
+					file: `${server.config.storage.dataRoot}/recording.txt`
+				} satisfies DvrWebhookPayload
+			});
+
+			expect(response.statusCode).toBe(400);
+			expect(response.json()).toStrictEqual({ code: 1 });
+
+			expect(mocks.existsSync).not.toHaveBeenCalled();
+		});
+
+		it("should fail if file doesn't exist", async () => {
+			mocks.existsSync.mockReturnValueOnce(false);
+
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body: {
+					...body,
+					file: `${server.config.storage.dataRoot}/app_id/stream_id/recording.flv`
+				} satisfies DvrWebhookPayload
+			});
+
+			expect(response.statusCode).toBe(400);
+			expect(response.json()).toStrictEqual({ code: 1 });
+
+			expect(mocks.existsSync).toHaveBeenCalledOnce();
+		});
+	});
+
+	describe('dvr param', () => {
+		const body = MockBody({
+			app: 'app_id',
+			stream: 'stream_id',
+			file: `${server.config.storage.dataRoot}/app_id/stream_id/recording.flv`
+		});
+
+		it('should skip upload with dvr=false', async () => {
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body: {
+					...body,
+					param: '?dvr=false'
+				} satisfies DvrWebhookPayload
+			});
+
+			expect(response.statusCode).toBe(200);
+			expect(response.json()).toStrictEqual({ code: 0 });
+
+			expect(mocks.s3Upload).not.toHaveBeenCalled();
+			expect(mocks.rm).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledWith(`${RECORDINGS_PATH}/app_id/stream_id/recording.flv`);
+		});
+
+		it('should upload with dvr=true', async () => {
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body: {
+					...body,
+					param: '?dvr=true'
+				} satisfies DvrWebhookPayload
+			});
+
+			await flushPromises();
+
+			expect(response.statusCode).toBe(200);
+			expect(response.json()).toStrictEqual({ code: 0 });
+
+			expect(mocks.s3Upload).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledWith(`${RECORDINGS_PATH}/app_id/stream_id/recording.flv`);
+		});
+
+		it('should skip upload with dvr=undefined', async () => {
+			const response = await server.inject({
+				method: 'POST',
+				url: '/v1/s3',
+				body
+			});
+
+			await flushPromises();
+
+			expect(response.statusCode).toBe(200);
+			expect(response.json()).toStrictEqual({ code: 0 });
+
+			expect(mocks.s3Upload).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledTimes(1);
+			expect(mocks.rm).toHaveBeenCalledWith(`${RECORDINGS_PATH}/app_id/stream_id/recording.flv`);
 		});
 	});
 });
